3bb237a95ba5f4ae563f84da1d8d1ee5605a9391
jiwicky <jiwicky@gmail.com>
2015-03-17 14:51:05 -0400

    seventh

--- templates/content.php
+++ templates/content.php
@@ -1,5 +1,6 @@
 <article <?php post_class(); ?>>
   <header>
+  <div class="latest-post-thumbnail"><?php the_post_thumbnail(175,175,true);?></div>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
