9e7774f91590efb05de22f3925b22a0f00e0b059
jiwicky <jiwicky@gmail.com>
2015-03-24 13:46:49 -0400

    eod

--- home-template.php
+++ home-template.php
@@ -12,6 +12,7 @@
 	<?php get_template_part('templates/covers-section'); ?>
 
 </div>
+
 <div class="col-md-4">
 
 <?php get_template_part('templates/logo');?>
--- templates/content-single.php
+++ templates/content-single.php
@@ -14,7 +14,7 @@ $args = array(
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-<div class="article">
+<div class="article col-md-8">
   <article <?php post_class(); ?>>
     <header>
       <img class="single-header-fork" src="<?php echo get_template_directory_uri();?>/assets/images/logo-fork-graphic.png" alt="" />
@@ -28,26 +28,23 @@ $args = array(
       
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
+
     <div class="entry-content">
       <?php the_content(); ?>
       <?php wp_link_pages($args); ?> 
     </div>
+
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
     </footer>
 
   </article>
 </div>
-
-<!-- <div class=" col-md-4 logo">
-  <?php get_template_part('templates/logo');?>
-</div>
-
-<div class="col-md-4 category-sidebar">
-  <?php get_template_part('templates/covers-section'); ?>
-</div> -->
-
 <?php endwhile; ?>
+
+<div class="col-md-4">
+  <?php get_template_part('templates/sidebar'); ?>
+</div>
 <div class="container col-md-12">
     <?php get_template_part('templates/bottom-posts');?>
 </div>
