c7e28e657806f37815a743f2cb2ac79c79760bab
jiwicky <jiwicky@gmail.com>
2015-03-24 11:04:18 -0400

    fix sidebar conditional statement

--- template-archive.php
+++ template-archive.php
@@ -8,4 +8,3 @@
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
-
--- templates/covers-section.php
+++ templates/covers-section.php
@@ -1,14 +1,14 @@
-<?php if( is_single())  {
-	echo '<div class="vertical-covers">';
-} else {
+<?php if(is_front_page()) {
 	echo '<div class="horizontal-covers">';
+} else {
+	echo '<div class="vertical-covers">';
 	}?>
 
 <?php foreach (get_categories() as $cat) : ?>
-	<?php if(is_single()) {
-	echo '<div class="vertical-cover-single col-md-10 col-md-offset-1">';
-} else {
+	<?php if(is_front_page()) {
 	echo '<div class="col-md-4 horizontal-cover-single">';
+} else {
+	echo '<div class="vertical-cover-single col-md-10 col-md-offset-1">';
 	}?> 
 
 <a href="<?php echo get_category_link($cat->term_id); ?>">
