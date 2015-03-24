bd69801125acec28e3a5f5adff5b58b2c86da8b4
jiwicky <jiwicky@gmail.com>
2015-03-24 10:02:35 -0400

    daily

--- templates/covers-section.php
+++ templates/covers-section.php
@@ -1,4 +1,4 @@
-<?php if(is_single()) {
+<?php if( is_single())  {
 	echo '<div class="vertical-covers">';
 } else {
 	echo '<div class="horizontal-covers">';
@@ -9,15 +9,14 @@
 	echo '<div class="vertical-cover-single col-md-10 col-md-offset-1">';
 } else {
 	echo '<div class="col-md-4 horizontal-cover-single">';
-	}?>
+	}?> 
+
 <a href="<?php echo get_category_link($cat->term_id); ?>">
 	<img src="<?php echo z_taxonomy_image_url($cat->term_id); ?>" />
 </a>
 
 <a href="<?php echo get_category_link($cat->term_id); ?>">
-
 	<?php echo $cat->cat_name; ?>
-
 </a>
 	</div>
 <?php endforeach; ?>
