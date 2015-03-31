1b3a020a792dad0d36b60281346b09718d4a4e38
jiwicky <jiwicky@gmail.com>
2015-03-31 16:44:13 -0400

    add

--- templates/forms.php
+++ templates/forms.php
@@ -1,5 +1,14 @@
 <div class="home-forms">
-  <div class="home-form">Advertise</div>
-  <div class="home-form">Contribute</div>
-  <div class="home-form">Recipes</div>
+  <div class="home-form">
+  	<h2>Advertise</h2>
+  	<p>Food Life Magazine reaches a lot of people that might be interested in your product or service. Contact us to see if we can help!</p>
+  </div>
+  <div class="home-form">
+  	<h2>Contribute</h2>
+  	<p>Have a story to tell? We would like to hear it!</p>
+  </div>
+  <div class="home-form">
+  	<h2>Recipes</h2>
+  	<p> Send us a favorite recipe for the next issue!</p>
+  </div>
 </div>
